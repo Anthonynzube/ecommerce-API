522b5783ff106c5c6e6d8415f8327068208cfc26
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-15 19:35:15 +0100

    Modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
             'discount' => $this->discount,
+            'totalPrice' => round($this->price - (($this->discount/100) * $this->price), 2),
+            'rating' => $this->reviews->count() >= 1 ? round($this->reviews->sum('star')/$this->reviews->count(), 1) : 'No rating yet',
+            'href' => [
+                'reviews' => route('review.index', $this->id),
+            ],
         ];
     }
 }
