2e2af3e77195a4cedcf95cb17247d0fe030288fe
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-18 20:13:55 +0100

    Minor Changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -55,6 +55,7 @@ class ProductController extends Controller
         return response([
             "data" => new ProductResource($product)
         ], Response::HTTP_CREATED);
+        
     }
 
     /**
