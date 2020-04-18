c0199dda338007957d0a75ff3956c647d229c561
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-18 20:11:58 +0100

    Create Products

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::Resource('/products', 'ProductController');
+Route::apiResource('/products', 'ProductController');
 
 Route::group(['prefix' => 'products'], function(){
 
