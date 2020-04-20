e983cfa43d9daad439c68b7b65beee891637f708
Anthonynzube <nzubeanthony4@gmail.com>
2020-04-20 14:21:02 +0100

    Customizing Exception Handlers

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
+/**
+ * 
+ */
+trait ExceptionTrait
+{
+    public function apiException($request, $e)
+    {
+        if ($this->isModel($e)) {
+            return $this->modelResponse($e);
+        }
+
+            if ($this->isHttp($e)) {
+                return $this->httpResponse($e);
+            }
+
+                if ($this->isMethod($e)) {
+                    return $this->methodResponse($e);
+                }
+
+                    return parent::render($request, $exception);
+
+    }
+
+    protected function isModel($e)
+    {
+        return $e instanceof ModelNotFoundException;
+    }
+
+    protected function isHttp($e)
+    {
+        return $e instanceof NotFoundHttpException;
+    }
+
+    protected function isMethod($e)
+    {
+        return $e instanceof MethodNotAllowedHttpException;
+    }
+
+    protected function modelResponse($e)
+    {
+        return response()->json([
+                'errors' => 'Product Model Not Found'
+            ], Response::HTTP_NOT_FOUND);
+    }
+
+    protected function httpResponse($e)
+    {
+        return response()->json([
+                'errors' => 'Route does not exist'
+            ], Response::HTTP_NOT_FOUND);
+    }
+
+    protected function methodResponse($e)
+    {
+        return response()->json([
+                'errors' => 'Method cannot be used on this route'
+            ], Response::HTTP_NOT_FOUND);
+    }
+}
--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,11 +5,11 @@ namespace App\Exceptions;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ExceptionTrait;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -55,18 +55,7 @@ class Handler extends ExceptionHandler
     {
         if ($request->expectsJson())
         {
-
-           if ($exception instanceof ModelNotFoundException) {
-                return response()->json([
-                    'errors' => 'Product Model Not Found'
-                ], Response::HTTP_NOT_FOUND);
-           }
-
-           if ($exception instanceof NotFoundHttpException) {
-                return response()->json([
-                    'errors' => 'Route does not exist'
-                ], Response::HTTP_NOT_FOUND);
-           }
+           return $this->apiException($request, $exception);
         }
         
         return parent::render($request, $exception);
